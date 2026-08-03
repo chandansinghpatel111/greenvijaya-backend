e82c7ed2db16075c17f9d938981635359767f1f5
deepu-1523 <chaurasiyad678@gmail.com>
2026-08-03 14:43:39 +0530

    Fix Vercel deployment: export app and add vercel.json for Express routing

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
+connectDB();
+
+if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+}
+
+module.exports = app;
