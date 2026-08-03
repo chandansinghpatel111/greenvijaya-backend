659c76995dced4d8b11a2e34b2e596176e4a6dd4
deepu-1523 <chaurasiyad678@gmail.com>
2026-08-03 16:03:07 +0530

    Fix unhandled promise rejection in server.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ const connectDB = async () => {
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    // process.exit(1); // Do not exit process in serverless environment
   }
 };
 
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+connectDB().catch(console.error);
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
